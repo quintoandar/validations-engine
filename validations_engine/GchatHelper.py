ecd51ba0273aeab6a63f241e66896e9da4363e10
Kleber Inoue <kleber.inoue@quintoandar.com.br>
2023-11-07 21:43:25 -0300

    adding gchathelper and message class

---
+++ validations_engine/GchatHelper.py
@@ -0,0 +1,37 @@
+from typing import List
+import requests
+import logging
+
+from validations_engine.message import Message
+
+
+class GChatService:
+    @staticmethod
+    def send_message(message: Message) -> bool:
+        """
+        Sends a message to a GChat webhook. Returns True if the message was sent
+        successfully, False otherwise.
+        """
+
+        payload = {"text": message.content}
+        response = requests.post(message.destination, json=payload)
+        try:
+            response.raise_for_status()
+        except Exception as e:
+            logging.warning(
+                f"m=send_message, msg=Gchat message was not sent, check"
+                f" the webhook url: {message.destination}, payload:{payload},"
+                f" error: {e}"
+            )
+            return False
+        return True
+
+    @staticmethod
+    def send_messages(messages: List[Message]) -> bool:
+        """
+        Sends a list of messages to a GChat webhook. Returns True if all messages
+        were sent successfully, False otherwise.
+        """
+
+        all_success = all([GChatService.send_message(message) for message in messages])
+        return all_success
